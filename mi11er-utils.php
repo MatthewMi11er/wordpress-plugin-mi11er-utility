c1f17001ab17c2a9cc28988d81be1d711f343390
Matthew Miller <matthew@mi11er.net>
2015-11-22 17:36:26 -0500

    empty plugin

---
+++ mi11er-utils.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Plugin Name: Blogger Redirect
+ * Plugin URI:
+ * Description: Redirects old blogger urls
+ * Author:
+ * Author URI:
+ * Version: 0.9.4
+ * License:
+ * License URI:
+ * @package mi11er_utils
+ */
+
+return;
