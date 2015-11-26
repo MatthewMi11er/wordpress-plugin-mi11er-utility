f26437fd61104d695f4e831f7721d4ad1c1306d8
Matthew Miller <matthew@mi11er.net>
2015-11-26 08:17:35 -0500

    remove old style links

--- includes/mi11er-utility/class-site-icons.php
+++ includes/mi11er-utility/class-site-icons.php
@@ -33,6 +33,9 @@ class Site_Icons
 {
 	/**
 	 * Print the favicon links
+	 * TODO: theme-color
+	 * TODO: maskicon color
+	 * TODO: File Names
 	 */
 	public static function the_icon_links() {
 		$msapplication_notification_polling_uris = apply_filters( 'mi11er_utility_favicon_msapplication_notification_urls', array( home_url( '/feed/' ) ) );
@@ -57,53 +60,10 @@ class Site_Icons
 		<link rel="icon" type="image/png" href="<?php TT::the_home_url( '/favicon-16x16.png' ); ?>" sizes="16x16">
 		<link rel="manifest" href="<?php TT::the_home_url( '/manifest.json' ); ?>">
 		<link rel="mask-icon" href="<?php TT::the_home_url( '/safari-pinned-tab.svg' ); ?>" color="#5bbad5">
-		<meta name="msapplication-TileColor" content="#da532c">
+		<meta name="msapplication-TileColor" content="<?php echo esc_attr( $msapplication_tile_color ); ?>">
 		<meta name="msapplication-TileImage" content="<?php TT::the_home_url( '/mstile-144x144.png' ); ?>">
 		<meta name="theme-color" content="#ffffff">
-		
-		
-		    <!-- ======================== END SITE ICONS ======================== -->
-		
-
-    <link rel="apple-touch-icon-precomposed" href="<?php TT::the_home_url( '/apple-touch-icon-precomposed.png' ); ?>" />
-    <link rel="apple-touch-icon-precomposed" sizes="57x57" href="<?php TT::the_home_url( '/apple-touch-icon-precomposed-57x57.png' ); ?>" />
-    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?php TT::the_home_url( '/apple-touch-icon-precomposed-72x72.png' ); ?>" />
-    <link rel="apple-touch-icon-precomposed" sizes="76x76" href="<?php TT::the_home_url( '/apple-touch-icon-precomposed-76x76.png' ); ?>" />
-    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?php TT::the_home_url( '/apple-touch-icon-precomposed-114x114.png' ); ?>" />
-    <link rel="apple-touch-icon-precomposed" sizes="120x120" href="<?php TT::the_home_url( '/apple-touch-icon-precomposed-120x120.png' ); ?>" />
-    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="<?php TT::the_home_url( '/apple-touch-icon-precomposed-144x144.png' ); ?>" />
-    <link rel="apple-touch-icon-precomposed" sizes="152x152" href="<?php TT::the_home_url( '/apple-touch-icon-precomposed-152x152.png' ); ?>" />
-    <!-- ie10 -->
-    <meta name="msapplication-TileColor" content="<?php echo esc_attr( $msapplication_tile_color ); ?>"/>
-    <meta name="msapplication-TileImage" content="<?php TT::the_home_url( '/windows-live-tile-144x144.png' ); ?>" />
-    <!-- /ie10 -->
-    <!-- recommended windows 8 pinned site icon sizes http://msdn.microsoft.com/en-us/library/dn455106(v=vs.85).aspx -->
-    <meta name="msapplication-square70x70logo" content="<?php TT::the_home_url( '/windows-live-tile-128x128.png' ); ?>" />
-    <meta name="msapplication-square150x150logo" content="<?php TT::the_home_url( '/windows-live-tile-270x270.png' ); ?>" />
-    <meta name="msapplication-square310x310logo" content="<?php TT::the_home_url( '/windows-live-tile-558x558.png' ); ?>" />
-    <meta name="msapplication-wide310x150logo" content="<?php TT::the_home_url( '/windows-live-tile-558x270.png' ); ?>" />
-    <meta name="msapplication-tooltip" content="<?php echo esc_attr( $msapplication_tooltip ); ?>"/>
-    <meta name="msapplication-notification" content="frequency=180;<?php self::the_msapplication_notification_polling_uris( $msapplication_notification_polling_uris ); ?>">
-    <meta name="application-name" content="<?php echo esc_attr( $msapplication_name ); ?>" />
-		
-		return $IND.'<!-- ======================== FAVICONS ======================== -->'.$NL
-					.$IND.'<link rel="apple-touch-icon" sizes="57x57" href="/apple-touch-icon-57x57.png">'.$NL
-					.$IND.'<link rel="apple-touch-icon" sizes="114x114" href="/apple-touch-icon-114x114.png">'.$NL
-					.$IND.'<link rel="apple-touch-icon" sizes="72x72" href="/apple-touch-icon-72x72.png">'.$NL
-					.$IND.'<link rel="apple-touch-icon" sizes="144x144" href="/apple-touch-icon-144x144.png">'.$NL
-					.$IND.'<link rel="apple-touch-icon" sizes="60x60" href="/apple-touch-icon-60x60.png">'.$NL
-					.$IND.'<link rel="apple-touch-icon" sizes="120x120" href="/apple-touch-icon-120x120.png">'.$NL
-					.$IND.'<link rel="apple-touch-icon" sizes="76x76" href="/apple-touch-icon-76x76.png">'.$NL
-					.$IND.'<link rel="apple-touch-icon" sizes="152x152" href="/apple-touch-icon-152x152.png">'.$NL
-					.$IND.'<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon-180x180.png">'.$NL
-					.$IND.'<link rel="icon" type="image/png" href="/favicon-192x192.png" sizes="192x192">'.$NL
-					.$IND.'<link rel="icon" type="image/png" href="/favicon-160x160.png" sizes="160x160">'.$NL
-					.$IND.'<link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96">'.$NL
-					.$IND.'<link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16">'.$NL
-					.$IND.'<link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32">'.$NL
-					.$IND.'<meta name="msapplication-TileColor" content="#da532c">'.$NL
-					.$IND.'<meta name="msapplication-TileImage" content="/mstile-144x144.png">'.$NL;
-		
+		<!-- ======================== END SITE ICONS ======================== -->
 <?php
 	}
 
