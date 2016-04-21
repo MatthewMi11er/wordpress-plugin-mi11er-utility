20573ee33c6c2180dabf204a250e6c7d4b180958
Matthew Miller <matthew@mi11er.net>
2016-04-21 08:08:00 -0400

    refactor autoloader

--- includes/mi11er-utility/class-autoloader.php
+++ includes/mi11er-utility/class-autoloader.php
@@ -29,12 +29,11 @@ class Autoloader
 		$parts = explode( '\\', strtolower( str_replace( '_', '-', $classname ) ) );
 
 		// Turn the parts into a path relative to this files dir.
-		$c = count( $parts );
-		$path = __DIR__;
-		for ( $i = 0; $i < $c -1; $i++ ) {
-			$path .= DIRECTORY_SEPARATOR . $parts[ $i ];
-		}
-		$path .= DIRECTORY_SEPARATOR . 'class-' . $parts[ $c -1 ] . '.php';
+		end( $parts );
+		$key = key( $parts );
+		$parts[ $key ] = 'class-' . $parts[ $key ] . '.php';
+		
+		$path = __DIR__ . DIRECTORY_SEPARATOR . implode( DIRECTORY_SEPARATOR, $parts );
 
 		// If the path is a file, we require it becuase it contains the class we want.
 		if ( is_file( $path ) ) {
